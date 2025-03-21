2d10eef2958132664544605773ef7ef6a7bc6789
Mettelund97 <lu.nd3@hotmail.com>
2025-03-21 15:52:07 +0100

    firebase install

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCwJ814_t4KfCnRUz0WMdOB31oDa9cEILg",
+    authDomain: "dbi-egenkontrol-mfd-958b1.firebaseapp.com",
+    projectId: "dbi-egenkontrol-mfd-958b1",
+    storageBucket: "dbi-egenkontrol-mfd-958b1.firebasestorage.app",
+    messagingSenderId: "295302196335",
+    appId: "1:295302196335:web:7339ca031904b14417b19a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
