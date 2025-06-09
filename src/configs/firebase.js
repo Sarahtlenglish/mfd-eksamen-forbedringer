d6610fa316eb7b87687f32a57c450f1be0bdc36b
Sarahtlenglish <sarah_english@live.dk>
2025-06-09 11:52:10 +0200

    import firestore

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,11 @@ const firebaseConfig = {
   appId: "1:673003719786:web:41e5d4660813c373110e13"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
