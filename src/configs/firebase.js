77677c67bfde15a06f10611430ebdb6cbb3e0c17
Sarahtlenglish <sarah_english@live.dk>
2025-06-09 11:37:21 +0200

    first commit

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCwJ814_t4KfCnRUz0WMdOB31oDa9cEILg",
-    authDomain: "dbi-egenkontrol-mfd-958b1.firebaseapp.com",
-    projectId: "dbi-egenkontrol-mfd-958b1",
-    storageBucket: "dbi-egenkontrol-mfd-958b1.firebasestorage.app",
-    messagingSenderId: "295302196335",
-    appId: "1:295302196335:web:7339ca031904b14417b19a"
+  apiKey: "AIzaSyDV-lauYhta94HjC13Ilgb82JSOaclFMI4",
+  authDomain: "dbi-forbedringer.firebaseapp.com",
+  projectId: "dbi-forbedringer",
+  storageBucket: "dbi-forbedringer.firebasestorage.app",
+  messagingSenderId: "673003719786",
+  appId: "1:673003719786:web:41e5d4660813c373110e13"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
